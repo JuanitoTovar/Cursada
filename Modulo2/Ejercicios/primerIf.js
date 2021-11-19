237c5f36724c6731d1e726d38fb7061a7ef35f3f
Juan de Jesus Tovar Vázquez <garfil34@gmail.com>
2021-11-19 08:33:06 -0600

    Se agregan mas ejercicios y la hora de notas.

---
+++ Modulo2/Ejercicios/primerIf.js
@@ -0,0 +1,18 @@
+/*
+Declaremos una variable con el nombre dato y 
+asignémosle un valor booleano.
+
+Luego, deberemos hacer un if que evalúe esa 
+variable y, si el valor es igual a  true, que 
+imprima por consola "es true". Caso contrario, 
+que imprima por consola "es false".
+
+*/
+let dato = false;
+
+if (dato) {
+    console.log('es true');
+}else{
+    console.log('es false');
+}
+
